9c21d532d40960cffb7511cf104bcb891b34b890
Evgeniy Aleshin <Sony@MacBook-Air.local>
2022-01-08 20:33:46 +0300

    Solved SearchInsertPosition task

---
+++ SearchInsertPosition.py
@@ -0,0 +1,6 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        pos = 0
+        while pos < len(nums) and nums[pos] < target:
+            pos += 1
+        return pos
