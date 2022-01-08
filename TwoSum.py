6e55321af07363997ab1e0a28c5c836bdbbb9a84
Evgeniy Aleshin <Sony@MacBook-Air.local>
2022-01-08 19:33:50 +0300

    Solved TwoSum task

--- TwoSum.py
+++ TwoSum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-	    n = len(nums)
-	    dic = {} 
-	    for i in range(0,n):
-		    if dic.get(target-nums[i])!=None:
-			    return [i,dic[target-nums[i]]];
-		    dic[nums[i]]=i
-	    return []
-        
+        n = len(nums)
+        dic = {}
+        for i in range(0, n):
+            if dic.get(target - nums[i]) != None:
+                return [i, dic[target - nums[i]]]
+            dic[nums[i]] = i
+        return []
