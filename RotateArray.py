98cef02829fe63cd09381c63419067eaf985ae21
Evgeniy Aleshin <Sony@MacBook-Air.local>
2022-01-30 19:20:15 +0300

    Solved RotateArray task

---
+++ RotateArray.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+	def twist(arr, i, j):
+            while (i < j):
+                arr[i], arr[j] = arr[j], arr[i]
+                i += 1
+                j -= 1
+            return arr
+        
+        if k > len(nums):
+            k %= len(nums)
+            
+        if (k > 0):
+            twist(nums, 0, len(nums) - 1)  
+            twist(nums, 0, k - 1)          
+            twist(nums, k, len(nums) - 1)  
